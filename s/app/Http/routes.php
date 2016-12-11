24295bd59e355b403ff48d7f90ed3de60786cf60
Jefferson Santos <jefflssantos@gmail.com>
2016-12-11 18:02:37 -0300

    xx

--- s/app/Http/routes.php
+++ s/app/Http/routes.php
@@ -4,6 +4,9 @@ use GuzzleHttp\Client;
 
 $client = new \GuzzleHttp\Client();
 
-$app->get('/', function () use ($app) {
-    return 'asd';
+$app->get('/', function () use ($app, $client) {
+    // dd(json_decode($client->get('http://s1.jeffsantos.com.br')->getBody()));
+    return response()->json(
+        json_decode($client->get('http://s1.jeffsantos.com.br')->getBody())
+    );
 });
