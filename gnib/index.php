9684d4ba30202246a207eae2b86ce1ffe7c5f7e6
Jefferson Santos <email@jeffsantos.com.br>
2017-07-06 20:21:25 +0100

    Update index.php

--- gnib/index.php
+++ gnib/index.php
@@ -23,40 +23,40 @@ function dump($value) {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
      <!-- Metas -->
     <meta name="description" content="GNIB Appointment"/>
-    <meta name="author" content="Jefferson Santos"/>
+    <meta name="author" content="GNIB Appointment by Jefferson Santos"/>
     <meta name="copyright" content="Santos Jefferson © 2017 Todos os Direitos Reservados"/>
     <meta name="application-name" content="jeffsantos.com.br"/>
 
-    <meta property="og:title" content="Jefferson Santos"/>
+    <meta property="og:title" content="GNIB Appointment by Jefferson Santos"/>
     <meta property="og:type" content="article"/>
-    <meta property="og:image" content="http://jeffsantos.com.br/img/jefflssantos.jpg"/>
-    <meta property="og:url" content="http://jeffsantos.com.br/"/>
+    <meta property="og:image" content="https://jeffsantos.com.br/img/jefflssantos.jpg"/>
+    <meta property="og:url" content="https://jeffsantos.com.br/"/>
     <meta property="og:description" content="GNIB Appointment"/>
     <meta property="article:author" content="https://www.facebook.com/Jeff.lssantos"/>
     <meta property="fb:admins" content="100001302304071"/>
 
     <meta name="twitter:card" content="summary"/>
-    <meta name="twitter:title" content="Jefferson Santos"/>
+    <meta name="twitter:title" content="GNIB Appointment by Jefferson Santos"/>
     <meta name="twitter:description" content="GNIB Appointment"/>
-    <meta name="twitter:image" content="http://jeffsantos.com.br/img/jefflssantos.jpg">
+    <meta name="twitter:image" content="https://jeffsantos.com.br/img/jefflssantos.jpg">
 
-    <link rel="apple-touch-icon" sizes="57x57" href="img/favicon/apple-touch-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="img/favicon/apple-touch-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="img/favicon/apple-touch-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-touch-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="img/favicon/apple-touch-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="img/favicon/apple-touch-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="img/favicon/apple-touch-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon-180x180.png">
-    <link rel="icon" type="image/png" href="img/favicon/favicon-32x32.png" sizes="32x32">
-    <link rel="icon" type="image/png" href="img/favicon/android-chrome-192x192.png" sizes="192x192">
-    <link rel="icon" type="image/png" href="img/favicon/favicon-96x96.png" sizes="96x96">
-    <link rel="icon" type="image/png" href="img/favicon/favicon-16x16.png" sizes="16x16">
-    <link rel="manifest" href="img/favicon/manifest.json">
-    <link rel="mask-icon" href="img/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="apple-touch-icon" sizes="57x57" href="https://jeffsantos.com.br/img/favicon/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="https://jeffsantos.com.br/img/favicon/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="https://jeffsantos.com.br/img/favicon/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="https://jeffsantos.com.br/img/favicon/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="https://jeffsantos.com.br/img/favicon/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="https://jeffsantos.com.br/img/favicon/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="https://jeffsantos.com.br/img/favicon/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="https://jeffsantos.com.br/img/favicon/apple-touch-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="https://jeffsantos.com.br/img/favicon/apple-touch-icon-180x180.png">
+    <link rel="icon" type="image/png" href="https://jeffsantos.com.br/img/favicon/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="https://jeffsantos.com.br/img/favicon/android-chrome-192x192.png" sizes="192x192">
+    <link rel="icon" type="image/png" href="https://jeffsantos.com.br/img/favicon/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="https://jeffsantos.com.br/img/favicon/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="https://jeffsantos.com.br/img/favicon/manifest.json">
+    <link rel="mask-icon" href="https://jeffsantos.com.br/img/favicon/safari-pinned-tab.svg" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="msapplication-TileImage" content="img/favicon/mstile-144x144.png">
+    <meta name="msapplication-TileImage" content="https://jeffsantos.com.br/img/favicon/mstile-144x144.png">
     <meta name="theme-color" content="#ffffff">
     
     <title>GNIB Appointment</title>
@@ -80,7 +80,13 @@ function dump($value) {
         </div>
       </div>
     </section>
-
-
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-99029729-1', 'auto');
+      ga('send', 'pageview');
+    </script>
   </body>
 </html>
